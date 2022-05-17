bcf55b080bca1378fcab552c64eb0737d29f1e2f
andreateruzzi <andreateruzzi98@gmail.com>
2022-05-17 15:58:12 +0200

    update

--- simulation/simulation.jl
+++ simulation/simulation.jl
@@ -1,6 +1,7 @@
 include("/Users/andreateruzzi/Desktop/combinatorial_auction_ILP/cap_juMP_julia/auction.jl") 
 using .CAP, JuMP, HiGHS, Plots
 
+
 n = 8             #number of items
 m = 2             #number of bidders
 subset = true     #restricting valuations to n^2 instead of 2^n
